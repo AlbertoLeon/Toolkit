65f498c7d9a7890a59f7ac613dab9a9682ecb9ef
daniel_mazzini <damazzini@hotmail.com>
2014-04-18 16:25:44 +0200

    Agregar ruleset propios

---
+++ Tokiota.Toolkit/SharedFiles/GlobalAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Resources;
+
+/* This file contains attributes that appear on every assembly - both
+ * product and test assemblies. */
+
+[assembly: AssemblyCompany("Tokiota - http://www.tokiota.com")]
+[assembly: AssemblyProduct("Tokiota toolkit")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
--- Tokiota.Toolkit/Tokiota.Toolkit.XCutting/IoC/IBuilder.cs
+++ Tokiota.Toolkit/Tokiota.Toolkit.XCutting/IoC/IBuilder.cs
@@ -3,42 +3,146 @@ using System.Reflection;
 
 namespace Tokiota.Toolkit.XCutting.IoC
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IBuilder
     {
         #region Public Methods and Operators
 
+        /// <summary>
+        /// Registers this instance.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <returns></returns>
         IBuilder Register<TClass>();
 
+        /// <summary>
+        /// Registers this instance.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <returns></returns>
         IBuilder Register<TInterface, TClass>() where TClass : TInterface;
 
-        IBuilder Register<TClass>(string name);
+        /// <summary>
+        /// Registers the specified name.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        IBuilder Register<TClass>(string name) where TClass : class;
 
+        /// <summary>
+        /// Registers the specified name.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
         IBuilder Register<TInterface, TClass>(string name) where TClass : TInterface;
 
+        /// <summary>
+        /// Registers as request scoped.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <returns></returns>
         IBuilder RegisterAsRequestScoped<TClass>();
 
+        /// <summary>
+        /// Registers as request scoped.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <returns></returns>
         IBuilder RegisterAsRequestScoped<TInterface, TClass>() where TClass : TInterface;
 
-        IBuilder RegisterAsRequestScoped<TClass>(string name);
+        /// <summary>
+        /// Registers as request scoped.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        IBuilder RegisterAsRequestScoped<TClass>(string name) where TClass : class;
 
+        /// <summary>
+        /// Registers as request scoped.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
         IBuilder RegisterAsRequestScoped<TInterface, TClass>(string name) where TClass : TInterface;
 
-        IBuilder RegisterAsSingleInstance<TClass>();
+        /// <summary>
+        /// Registers as single instance.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <returns></returns>
+        IBuilder RegisterAsSingleInstance<TClass>() where TClass : class;
 
+        /// <summary>
+        /// Registers as single instance.
+        /// </summary>
+        /// <param name="implementation">The implementation.</param>
+        /// <param name="interfaces">The interfaces.</param>
+        /// <returns></returns>
         IBuilder RegisterAsSingleInstance(Type implementation, Type[] interfaces);
 
+        /// <summary>
+        /// Registers as single instance.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <returns></returns>
         IBuilder RegisterAsSingleInstance<TInterface, TClass>() where TClass : TInterface;
 
+        /// <summary>
+        /// Registers as single instance.
+        /// </summary>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
         IBuilder RegisterAsSingleInstance<TClass>(string name);
 
+        /// <summary>
+        /// Registers as single instance.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
         IBuilder RegisterAsSingleInstance<TInterface, TClass>(string name) where TClass : TInterface;
 
+        /// <summary>
+        /// Registers the assembly.
+        /// </summary>
+        /// <param name="assemblyNames">The assembly names.</param>
+        /// <returns></returns>
         IBuilder RegisterAssembly(params string[] assemblyNames);
 
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="TClass">The type of the class.</typeparam>
+        /// <param name="instance">The instance.</param>
+        /// <returns></returns>
         IBuilder RegisterInstance<TInterface, TClass>(TClass instance) where TClass : class, TInterface;
 
+        /// <summary>
+        /// Registers the instance.
+        /// </summary>
+        /// <param name="instance">The instance.</param>
+        /// <returns></returns>
         IBuilder RegisterInstance(object instance);
 
+        /// <summary>
+        /// Registers the type of the types in assembly filter by interface.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <param name="filterName">Name of the filter.</param>
+        /// <returns></returns>
         IBuilder RegisterTypesInAssemblyFilterByInterfaceType(Assembly assembly, Type filterName);
 
         #endregion
