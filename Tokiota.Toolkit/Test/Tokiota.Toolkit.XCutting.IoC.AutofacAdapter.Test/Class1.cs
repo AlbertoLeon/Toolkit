917c955dbb0a342201f78300343e89dff9520ae3
daniel_mazzini <damazzini@hotmail.com>
2014-04-19 03:44:40 +0200

    compilar test package nuget

---
+++ Tokiota.Toolkit/Test/Tokiota.Toolkit.XCutting.IoC.AutofacAdapter.Test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit;
+using Xunit.Should;
+
+namespace Tokiota.Toolkit.XCutting.IoC.AutofacAdapter.Test
+{
+    
+    public class Resolved
+    {
+        [Fact]
+        public void FactMethodName()
+        {
+            bool x = true;
+
+            x.ShouldBeTrue();
+        }
+    }
+}
