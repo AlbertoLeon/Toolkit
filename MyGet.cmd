6f19bacb09af5e0e2b8fd1cbd10e08baeffd39bf
daniel_mazzini <damazzini@hotmail.com>
2014-04-25 16:26:16 +0200

    nuevo cambio en build

--- MyGet.cmd
+++ MyGet.cmd
@@ -1,2 +1,4 @@
 @echo off
-@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild CI_Main.build
+@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild CI_Main.build /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+pause
